4f715439780f1292a7cbe5b7204c85fe75bbb5bc
Alex <Alex@Alexs-MacBook-Pro.local>
2017-04-29 18:06:43 -0400

    First Website Upload

---
+++ scripts/scripts.js
@@ -0,0 +1,34 @@
+$(function() {
+   $('body').fadeIn(1500);
+});
+
+$("#top-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#top").offset().top},
+        'slow');
+});
+
+$("#about-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        'slow');
+});
+
+$("#work-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#work").offset().top},
+        'slow');
+});
+
+$("#photography-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#photography").offset().top},
+        'slow');
+});
+
+$("#contact-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+});
+
